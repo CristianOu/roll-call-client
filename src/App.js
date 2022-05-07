a0af4cd3bd7572f0e23925f8e2529ec70ff803df
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-07 20:20:21 +0200

    Include the Statistics Page into the router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import {useEffect, useState} from 'react';
 import './App.css';
 import http from "./services/http.service";
 import StatisticsPage from "./StatisticsPage";
+import SideBar from './components/side-bar/SideBar';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import RollCall from './pages/RollCall';
 
 function App() {
 
-    const [data, setData] = useState(0);
-        
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    http.get("/").then(res => {
+      setData(res.data);
+    })
+  }, []);
+
   return (
     <div className="App">
       <SideBar className='side-bar-column'/>
       <Router>
         <Routes>
-          <Route path="/" element={<RollCall className='roll-call-page-container' />}/>          
+          <Route path="/" element={<RollCall className='roll-call-page-container' />}/>
+          <Route path="/statistics" element={<StatisticsPage />}/>
         </Routes>
       </Router>
     </div>
