efbff410b1b4aff562580220a17ea99702e22913
dim <dimitris.giokas1@gmail.com>
2022-05-08 23:27:13 +0200

    added margin top

--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -222,7 +222,7 @@ function StatisticsPage() {
         }
 
         return (
-            <div style={{marginLeft: '20px', marginRight: '20px'}}>
+            <div style={{marginLeft: '20px', marginRight: '20px', marginTop:'10px'}}>
                 {isTeacher && <Dropdown title={'Course'} handleChange={handleCourseChange} options={courses}/>}
                 {isTeacher &&
                 <div style={{
