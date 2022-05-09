86e672b6634fe6a8005c0ceb615e31145e8dd278
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-09 03:23:04 +0200

    Working Sockets (I hope..)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import http from './services/http.service';
 import StatisticsPage from './pages/StatisticsPage';
 import SideBar from './components/side-bar/SideBar';
-import "@fontsource/plus-jakarta-sans"; // Defaults to weight 400.
+import '@fontsource/plus-jakarta-sans'; // Defaults to weight 400.
 import { Route, Routes, Navigate } from 'react-router-dom';
 import RollCall from './pages/RollCall';
 import LogInPage from './pages/LogInPage';
@@ -13,32 +13,33 @@ import socketIOClient from 'socket.io-client';
 const ENDPOINT = 'http://localhost:8080/';
 const socket = socketIOClient(ENDPOINT);
 
-const generateCode = (teacherId) => {
-  teacherId = 1;
-  //console.log("generate code", teacherId);
-  socket.emit('generateCode', teacherId);
-};
-
-const joinClass = (code, student) => {
-  code = document.getElementById('code').value;
-  student = {
-    studentId: 11,
-    firstName: 'Ondrej',
-    lastName: 'Surname',
-    age: 15,
-    email: 'name.surname@gmail.com'
-  };
-  //console.log(student);
-  //studentId = 10;
-  socket.emit('attendLecture', { code, student });
-};
-
 function App() {
   const [data, setData] = useState(0);
   const [students, setStudents] = useState([]);
   const [code, setCode] = useState('');
 
   const [session, setSession] = useState({});
+
+  const generateCode = (teacherId) => {
+    teacherId = 1;
+    //console.log("generate code", teacherId);
+    socket.emit('generateCode', teacherId);
+  };
+
+  const joinClass = (code, student) => {
+    code = document.getElementById('code').value;
+
+    student = {
+      studentId: session.userId,
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 15,
+      email: session.email
+    };
+    //console.log(student);
+    //studentId = 10;
+    socket.emit('attendLecture', { code, student });
+  };
 
   useEffect(() => {
     socket.on('codeGenerated', (data) => {
@@ -63,52 +64,83 @@ function App() {
 
     socket.on('studentJoined', (data) => {
       console.log('student joined', data);
-      setStudents([...students, data]);
+      setStudents((students) => [...students, data]);
     });
   }, []);
 
   useEffect(() => {
-    http.get('/').then((res) => {
-      setData(res.data);
-    });
+    // http.get('/').then((res) => {
+    //   setData(res.data);
+    // });
     async function fetchSession() {
       try {
         const response = await http.get('/getsession');
-        setSession(response.data)
+        setSession(response.data);
       } catch (error) {
         console.log(error);
-      } 
+      }
     }
     fetchSession();
   }, []);
 
-  console.log(students);
+  console.log('students', students);
 
   console.log('session', session);
+
   return (
-    <div className='App'>
-      {
-        session.userId ?
-          <SideBar className='side-bar-container' /> :
-          ""
-      }
-        <Routes>
-          <Route path="/" element={ session.userId ? (<RollCall className='roll-call-page-container' generateCode={generateCode} joinClass={joinClass} students={students} code={code} />) : (<LogInPage className={'login-container'} setSession={setSession}/>)}/>
-          <Route path="/statistics" element={ session.userId ? (<StatisticsPage />) : (<LogInPage className={'login-container'} setSession={setSession}/>)} />
-          <Route path="/login" element={ session.userId ? (<Navigate to="/" />) : (<LogInPage className={'login-container'} setSession={setSession}/>)}/>
-          <Route
-            path="*"
-            element={
-              session.userId ? (
-                <main style={{ padding: "1rem" }}>
-                  <p>There's nothing here for now!</p>
-                </main>
-              ) : (
-                <LogInPage className={'login-container'} setSession={setSession} />
-              )
-              
-            }/>
-        </Routes>
+    <div className="App">
+      {session.userId ? <SideBar className="side-bar-container" /> : ''}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            session.userId ? (
+              <RollCall
+                className="roll-call-page-container"
+                generateCode={generateCode}
+                joinClass={joinClass}
+                students={students}
+                code={code}
+                loggedInUser={session}
+              />
+            ) : (
+              <LogInPage className={'login-container'} setSession={setSession} />
+            )
+          }
+        />
+        <Route
+          path="/statistics"
+          element={
+            session.userId ? (
+              <StatisticsPage />
+            ) : (
+              <LogInPage className={'login-container'} setSession={setSession} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            session.userId ? (
+              <Navigate to="/" />
+            ) : (
+              <LogInPage className={'login-container'} setSession={setSession} />
+            )
+          }
+        />
+        <Route
+          path="*"
+          element={
+            session.userId ? (
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing here for now!</p>
+              </main>
+            ) : (
+              <LogInPage className={'login-container'} setSession={setSession} />
+            )
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/pages/RollCall.jsx
+++ src/pages/RollCall.jsx
@@ -3,10 +3,15 @@ import TopBar from '../components/top-bar/TopBar';
 import UtilityBar from '../components/utility-bar/UtilityBar';
 import StudentBox from '../components/student-box/StudentBox';
 
-function RollCall({ className, generateCode, joinClass, students, code }) {
+function RollCall({ className, generateCode, joinClass, students, code, loggedInUser }) {
   return (
     <div className={className}>
-      <TopBar generateCode={generateCode} joinClass={joinClass} code={code} />
+      <TopBar
+        generateCode={generateCode}
+        joinClass={joinClass}
+        code={code}
+        loggedInUser={loggedInUser}
+      />
 
       <UtilityBar />
 
