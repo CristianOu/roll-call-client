ef4ac3172fdf18364da0f02e8f91e533ea4a8191
Cristian Ou <mccrys_00@yahoo.com>
2022-05-15 12:45:55 +0200

    Added education name

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -3,8 +3,12 @@ import AuthenticationForm from '../components/authentication-form/Authentication
 
 
 function AuthenticationPage({setSession, isSignUp}) {
+
+  const nameOfEducation = 'Københavns Erhvervsakademi / Copenhagen School of Design and Technology';
+
   return (
     <div className='authentication-page-container'>
+      <div className='education-name'>{nameOfEducation}</div>
       <AuthenticationForm setSession={setSession} isSignUp={isSignUp} />
     </div>
   );
