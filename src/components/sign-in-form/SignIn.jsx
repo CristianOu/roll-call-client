1aca1e00995f6238becb7ef389b9cfaf0669077d
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-17 09:05:06 +0200

    useAuth() hook for authContext logic & Navigation post-auth

--- src/components/sign-in-form/SignIn.jsx
+++ src/components/sign-in-form/SignIn.jsx
@@ -1,63 +1,62 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import './SignIn.scss';
-import { Context as AuthContext} from '../../contexts/AuthContext';
+import useAuth from '../../hooks/useAuth';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function SignIn() {
-  const {state, signIn, clearErrorMessage} = useContext(AuthContext);
+  const { state, signIn, clearErrorMessage } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showError, setShowError] = useState(false);
 
-  const handleSignIn = async(e) => {
+  const handleSignIn = async (e) => {
     e.preventDefault();
-    const isError = signIn({email, password});
+    const result = signIn({ email, password, navigate, from });
 
-    if(isError) {
+    if (result) {
       setShowError(true);
-      setTimeout(() => {
-        setShowError(false);
-      }, 2500);
       clearErrorMessage();
     }
-  }
+  };
 
-  // console.log(state)
+  // console.log(state);
 
   return (
-    <div className='sign-in-wrapper'>
+    <div className="sign-in-wrapper">
       <div className="title">Sign In</div>
       <form onSubmit={handleSignIn}>
-        <label className='field'>
+        <label className="field">
           <div>Email</div>
           <input
-          name='email'
-          type={'email'}
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
-        />
+            name="email"
+            type={'email'}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
         </label>
-        
-        <label className='field'>
+
+        <label className="field">
           <div>Password</div>
           <input
-            name='password'
+            name="password"
             type={'password'}
             value={password}
             onChange={(event) => setPassword(event.target.value)}
             required
           />
         </label>
-        
 
         <button type="submit" value="submit-form">
           Sign In
         </button>
-
-        
       </form>
-      { showError ? <div className='error'>{state.errorMessage}</div> : '' }
+      {showError ? <div className="error">{state.errorMessage}</div> : ''}
     </div>
   );
 }
