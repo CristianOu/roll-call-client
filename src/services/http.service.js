d78746ecd8775ac9dbdf7b774f8840931e9a9e94
Cris <mccrys_00@yahoo.com>
2022-05-29 17:46:48 +0200

    Changed axios url

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_DEV_LOCAL,
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: process.env.REACT_APP_DEV_LOCAL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application.json' },
   withCredentials: true
 });
