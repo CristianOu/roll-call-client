8f38c7c0ce67e95458c8dfb5b4d02ad3daf50a72
Cristian Ou <mccrys_00@yahoo.com>
2022-06-05 12:50:26 +0200

    Clean up start class test

--- cypress/e2e/startClass.cy.js
+++ cypress/e2e/startClass.cy.js
@@ -3,26 +3,16 @@ import http from '../../src/services/http.service';
 describe ('Start a class', () => {
 
     it.only("start a class", () => {
-      login("John", "Doe", "TEACHER", "v-kane@yahoo.com", "JmE95osSMM4bYF", "1979-01-01", null);
+      login("v-kane@yahoo.com", "JmE95osSMM4bYF");
       selectAndStartClass();
     })
 })
 
-function login(firstName, lastName, userRole, email, password, dateOfBirth, classId) {
-  cy.visit('http://localhost:3000/login')
-    .then(async() => {
-      await executeApiLogin(firstName, lastName, userRole, email, password, dateOfBirth, classId)
-    });
+function login(email, password) {
+  cy.visit('http://localhost:3000/login');
   cy.get("input[type='email']").type(email);
   cy.get("input[type='password']").type(password);
   cy.get("button[type='submit']").click();
-}
-
-async function executeApiLogin(firstName, lastName, userRole, email, password, dateOfBirth, classId) {
-  await http.post(`http://localhost:8080/api/users/login`, {
-    email: email,
-    password: password
-  })
 }
 
 function selectAndStartClass() {
