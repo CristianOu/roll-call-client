c1e2f47a19c5cd6373cdb6d8cfb24f5fc73726a0
dim <dimitris.giokas1@gmail.com>
2022-06-05 07:39:14 +0200

    editd api.js

--- src/services/api.js
+++ src/services/api.js
@@ -27,5 +27,5 @@ export const getSignIn = (email, password) => {
 // fetches data from: https://confluence.govcloud.dk/display/FDAPI/Danish+Meteorological+Institute+-+Open+Data
 export const getWeatherData = () => {
     const apiKey = 'df4bf8b8-a641-459f-bf69-36c1b8de3ceb';
-    return http.get(`https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=${apiKey}&period=latest&bbox=12.5,55.6,12.6,55.7&parameterId=temp_dry`)
+    return http.get(`https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?api-key=${apiKey}&period=latest&bbox=12.5,55.6,12.6,55.7&parameterId=temp_dry`, { withCredentials: false })
 }
