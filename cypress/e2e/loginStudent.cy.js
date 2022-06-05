e106214b4bfb3b9eb906efa32e7bebc802904598
Marianna <mana998@azet.sk>
2022-06-05 13:47:23 +0200

    Added page elements assertions to login e2e

--- cypress/e2e/loginStudent.cy.js
+++ cypress/e2e/loginStudent.cy.js
@@ -11,12 +11,20 @@ function login(firstName, lastName, userRole, email, password, dateOfBirth, clas
   cy.visit('http://localhost:3000/login').then(async () => await register(firstName, lastName, userRole, email, password, dateOfBirth, classId));
   cy.get("input[type='email']").type(email);
   cy.get("input[type='password']").type(password);
+  cy.get("button[type='submit']").click();
+  //check whether neccessary data is loaded
+  cy.get("div[class='side-bar-container']");
+  cy.get("div[class='options-container']");
+  cy.get("div[class='logo']");
+  cy.get("div[class='top-bar-container']");
+  cy.get("div[class='utility-bar']");
+  cy.get("div[class='students-section empty']");
 }
 
 function logout() {
-  cy.get("button[type='submit']").click();
   cy.get("img[id='profilePicture']").click();
   cy.get("div[id='signOut']").click();
+  cy.get("div[class=authentication-form-container]");//should include this div after logout
 }
 
 async function register(firstName, lastName, userRole, email, password, dateOfBirth, classId) {
